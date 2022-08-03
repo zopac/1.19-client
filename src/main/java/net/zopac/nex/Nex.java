4cc195fdecec504f34447386127c8a36f913f544
zopac <realzopac@gmail.com>
2022-08-02 23:34:54 -0500

    add things

--- src/main/java/net/zopac/nex/Nex.java
+++ src/main/java/net/zopac/nex/Nex.java
@@ -1,6 +1,12 @@
 package net.zopac.nex;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Session;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,12 +16,16 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+import java.awt.*;
+
 // The value here should match an entry in the META-INF/mods.toml file
+// VERY IMPORTANT DETAIL IF WE CHANGE NAME YOU HAVE TO CHANGE IT IN BUILD.GRADLE AND MODS.TOML OR MOD WONT WORK
 @Mod(Nex.MOD_ID)
-public class Nex
-{
+public class Nex {
+
     public static final String MOD_ID = "nex";
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static Minecraft mc = Minecraft.getInstance();
 
     public Nex() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -27,12 +37,11 @@ public class Nex
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-
+            LOGGER.debug("Loaded as player: " + mc.getUser().getName());
         }
     }
 }
